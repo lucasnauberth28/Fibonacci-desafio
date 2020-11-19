3c1eca05ca9bddf2b865b1b7e32e3a02170608e3
lucasnauberth28 <lucasnauberth@gmail.com>
2020-11-19 13:37:34 -0300

    Fibonacci concluído

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonacci_desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero :");
+            int num;
+                int x=1, y=0, z=0;
+                    num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++){
+            z = x + y;
+            Console.Write( z + " ,");
+            y = x;
+            x = z;
+                }
+                Console.ReadLine();
+        }
+    }
+}
+
